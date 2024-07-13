f659796d79ad92a73e3432b255ce15ab707213ee
Bhavya Garg <113656564+Bhavya091202@users.noreply.github.com>
2024-07-13 17:17:33 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ import 'dotenv/config';
 const connectToMongo = async () => {
     try {
         // await connect(`mongodb+srv://bhavya54321:JvAUQJgCCYdLGK7i@merndb-0.snuwwf5.mongodb.net/eNotebook`);
-        await connect(`${process.env.MONGO_URI}`);
+        await connect(process.env.MONGO_URI);
         console.log('---***Database Connected Successfully***---');
     } catch (error) {
         console.log('---***Database Connection Failed***---');
